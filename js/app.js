06942aa47804ca85a6b48fe9cee21aca4e0c7362
qyzhencharles <qyzhencharles@gmail.com>
2022-05-02 22:55:31 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -38,13 +38,14 @@ function isInViewport(element) {
     rect.top >= 0 && rect.left >=0;
 }
 
-function lists() {
+function createNavItem() {
     sections.forEach(section => {
         let navItem = document.createElement('li');
         let navLink = document.createElement('a');
         // This is used to locate the section
-        navLink.setAttribute('href', `#${section.id}`);  
-        navLink.textContent = section.dataset.header;
+        // When being clicked, scroll to the specific section
+        navLink.setAttribute('href', `#${section.id}`);
+        navLink.textContent = section.dataset.nav;
         navItem.appendChild(navLink);
         navBar.appendChild(navItem);
         // Listen for a click event and scroll smoothly to the section
@@ -65,30 +66,36 @@ function lists() {
 
 // build the nav
 
-const navBar = document.querySelector('#navbar__list');
+const navBar = document.querySelector('.navbar__menu #navbar__list');
 const sections = document.querySelectorAll('section');
 // use data attributes to save the header content into the section's attribute
 sections.forEach(section => {
     const header = section.querySelector('h2');
     section.setAttribute('data-nav', header.textContent)
 })
-lists();
+createNavItem();
 
 
 
 
 
-for (let section of sections) {
-    const header = section.querySelector('h2')
-    const item = document.createElement('li');
-    const anchor = document.createElement('a');
-    anchor.classList.add("menu__link");
-    item.textContent = nav.textContent;
-    anchor.appendChild(item);
-    navBar.appendChild(anchor);
-}
+// for (let section of sections) {
+//     const header = section.querySelector('h2')
+//     const item = document.createElement('li');
+//     const anchor = document.createElement('a');
+//     anchor.classList.add("menu__link");
+//     item.textContent = nav.textContent;
+//     anchor.appendChild(item);
+//     navBar.appendChild(anchor);
+// }
+
+
+
+
 
 // Add class 'active' to section when near top of viewport
+document.addEventListener('scroll', active);
+
 function active() {
     for (let section of sections) {
         if(isInViewport(section)) {
